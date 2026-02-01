727940ad5355ac58306b995025ae03efd870673d
dsw7 <dds3443@hotmail.com>
2026-02-01 04:45:28 -0800

    Autogenerate latex

---
+++ commands/subcommand_generate_latex.py
@@ -0,0 +1,54 @@
+from subprocess import run
+from core.consts import OutputDirectory, PlotsDirectory
+
+LaTeXDirectory = OutputDirectory / "latex"
+
+
+def _generate_preamble() -> str:
+    return r"""\documentclass[10pt]{article}
+
+\usepackage{courier}
+\usepackage{geometry}
+\usepackage{graphicx}
+\usepackage{titlesec}
+\usepackage{datetime}
+
+\geometry{margin=1in}
+\titleformat{\section}{\bfseries\large}{}{0em}{}[\titlerule]
+\renewcommand*\familydefault{\ttdefault}
+\graphicspath{{output/plots}}
+"""
+
+
+def _generate_body() -> str:
+    return r"""
+\begin{document}
+\tableofcontents
+\newpage
+
+\section{Technical details}
+This report was acquired on \today\ at \currenttime.
+
+\end{document}
+"""
+
+
+def main() -> None:
+    if not LaTeXDirectory.exists():
+        LaTeXDirectory.mkdir()
+
+    latex_file = LaTeXDirectory / "report.tex"
+    markup = _generate_preamble() + _generate_body()
+    latex_file.write_text(markup)
+
+    command = [
+        "pdflatex",
+        "-interaction=nonstopmode",
+        f"-output-directory={LaTeXDirectory}",
+        f"{latex_file}",
+    ]
+    run(command, check=True)
+
+
+if __name__ == "__main__":
+    main()
